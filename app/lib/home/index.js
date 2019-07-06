c9dd21262d3ab1041299462ca773b4b1e7b53cc9
Adrian <kileras@gmail.com>
2019-07-06 16:46:22 +0100

    Added first bits of logic to get all podcasts

--- app/lib/home/index.js
+++ app/lib/home/index.js
@@ -1,7 +1,40 @@
+import {formatPodcastEntry} from '../util/itunes.js';
+
+const ITUNES_URL = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
+
+const template = document.querySelector('#podcast-tile');
 
 export const PATH = /^\/$/;
 export const ID = 'home';
 
-export const load = () => {
-	console.log('Load 2');
+const createTile = (fragment, entry) => {
+
+	const data = formatPodcastEntry(entry);
+
+	const tile = document.importNode(template.content, true);
+
+	const link = tile.querySelector('h2 a');
+	link.href = '/podcast/' + data.id;
+	link.textContent = data.name;
+
+	const author = tile.querySelector('.author');
+	author.textContent = data.author;
+
+	const img = tile.querySelector('img');
+	img.src = data.image;
+
+	fragment.append(tile);
+	return fragment;
+};
+
+export const load = async () => {
+	const res = await fetch(ITUNES_URL);
+	const {feed} = await res.json();
+	const {entry: entries} = feed;
+
+	const fragment = entries.reduce(createTile, document.createDocumentFragment());
+
+	const list = document.querySelector('#home .podcast-list');
+	list.innerHTML = '';
+	list.appendChild(fragment);
 };
