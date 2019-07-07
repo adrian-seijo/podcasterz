86944a29514d45fb52fbfcdce88e11b507cad282
Adrian <kileras@gmail.com>
2019-07-07 23:32:35 +0100

    Added missing alt

--- app/lib/views/common.js
+++ app/lib/views/common.js
@@ -14,7 +14,7 @@ export const renderPodcastDetail = (podcast) => {
 	} = podcast;
 
 	updateElements([
-		{selector: 'aside img', attrs: {src: image}},
+		{selector: 'aside img', attrs: {src: image, alt: title + ' logo'}},
 		{selector: 'aside h2', attrs: {textContent: title}},
 		{selector: 'aside .author', attrs: {textContent: author}},
 		{selector: 'aside .summary', attrs: {textContent: summary}},
